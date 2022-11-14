d7bd87816683115d46e0174cf4c7affa8d65fbdd
JoseAngel99 <j.lafferte@alumnos.santotomas.cl>
2022-11-14 16:35:21 -0300

    Initial commit

---
+++ app/src/main/java/com/programacionandroid/practica_sqlite/entidades/MainActivity.java
@@ -0,0 +1,16 @@
+package com.programacionandroid.practica_sqlite.entidades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.programacionandroid.practica_sqlite.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
